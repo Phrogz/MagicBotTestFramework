e7c0403536091cc8479c21e55eb7b77f915d0c89
Gavin Kistner <gavin@phrogz.net>
2025-10-20 23:18:42 -0600

    Make Ruff more persnickety

--- components/shooter.py
+++ components/shooter.py
@@ -1,29 +1,36 @@
+"""A hypothetical shooter component on the robot."""
+
 import magicbot
 import wpilib
 
 
 class Shooter:
-    """Pew! Pew!"""
+    """The robot's shooter.
+
+    Pew! Pew!
+    """
 
     shooter_motor: wpilib.Talon
 
-    # speed is tunable via NetworkTables
+    # Shooter motor speed is tunable via NetworkTables
     shoot_speed = magicbot.tunable(1.0)
 
     def __init__(self):
+        """Code to run when initially creating the shooter."""
         self.enabled = False
 
     def enable(self):
-        """Causes the shooter motor to spin"""
+        """Causes the shooter motor to spin."""
         self.enabled = True
 
     def is_ready(self):
+        """Determine if the shooter is ready to fire."""
         # in a real robot, you'd be using an encoder to determine if the
         # shooter were at the right speed..
         return True
 
     def execute(self):
-        """This gets called at the end of the control loop"""
+        """This gets called at the end of the control loop."""
         if self.enabled:
             self.shooter_motor.set(self.shoot_speed)
         else:
